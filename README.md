c9cd7e7f92ad8a0094276f867098b3b68ffd9ea9
Angelica Hanley <angelicahanley2@gmail.com>
2026-04-01 16:29:33 -0400

    max value and readme

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# COP4533Project3
+**Name**: Angelica Hanley, **UFID**: 85716778
+
+**Name**: Mikayla Cohen, **UFID**: 62308954
+
+Description:
+- This program implements the highest value longest common sequence (HVLCS). It computes a common subsequence of A and B that maximizes the total value, and outputs both this maximum value and the corresponding subsequence.
+
+Instructions to compile and run the code:
+- Clone the repository
+- Navigate to the project root directory 
+- In src/main.py, set the filename inside open() to the wanted input file
+- Run the program using python src/main.py
+    - If your system uses "py", run: py src/main.py
+    - If your system uses "python3", run: python3 src/main.py
+
+Assumptions:
+Input
+- The first number in the input file is K, the number of characters in the alphabet
+- Rows afterward should follow this format:
+    - x1 v1
+    - x2 v2
+    - . . .
+    - xK vK
+- The 2nd to last row is A, the first string
+- The last row is B, the second string
+
+Output
+- The first number in the output file is the maximum value found
+- The second row is the corresponding subsequence
+
+
+Question 1:
+
+
+Question 2:
+
+
+Question 3:
 
--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 
-
 def main():
     #reading from file
     try:
-        #alphabet parsing
+        #parsing
         with open("data/example.in","r") as file:
-            #reads each line into array lines
+            #reads each line into list
             lines= [line.strip() for line in file if line.strip()]
         #1st line becomes int k (alphabet length)
         k=int(lines[0])
@@ -16,21 +15,49 @@ def main():
             values[char]=int(value)
         A=lines[k+1]
         B=lines[k+2]
-        print(values)
-        print(A)
-        print(B)
+        #testing below
+        #print(values)
+        #print(A)
+        #print(B)
 
         #strings length, for dynamic programming table
-        n=len(A)
-        m=len(B)
+        stringALen=len(A)
+        stringBLen=len(B)
         #creating 2D dynamic prog array
         dynamicProg=[]
-        for i in range(n+1):
+        for i in range(stringALen+1):
             row=[]
-            for j in range(m+1):
+            for j in range(stringBLen+1):
                 row.append(0)
             dynamicProg.append(row)
-        print(dynamicProg)
+
+        #filling array
+        #skips base case row & column
+        for i in range(1,stringALen+1):
+            for j in range(1,stringBLen+1):
+                #checks if current chars match
+                if A[i-1]==B[j-1]:
+                    #3 options, skips char A, char B, or uses matching char and adds its value
+                    dynamicProg[i][j]=max(
+                        dynamicProg[i-1][j],
+                        dynamicProg[i][j-1],
+                        dynamicProg[i-1][j-1]+values[A[i-1]]
+                    )
+                    #if they don't match, skips 1 char (uses max)
+                else:
+                    dynamicProg[i][j]=max(
+                        dynamicProg[i-1][j],
+                        dynamicProg[i][j-1]
+                    )
+        #print(dynamicProg)
+        #is the optimal value for the 2 strings since bottom right array value stores this
+        #-> max value of a common subsequence
+        print(dynamicProg[stringALen][stringBLen])
+
+        #TO DO: trace backward through 2d array for actual solution (cb)
+        #so, one optimal common subsequence that achieves this value. 
+        #If multiple optimal subsequences exist, you may output any one of them
+
     except FileNotFoundError:
         print("File opening error")
         return
