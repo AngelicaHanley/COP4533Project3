28e3436f1b72e50f65717a10b2c0911f6808c89a
Angelica Hanley <angelicahanley2@gmail.com>
2026-04-01 12:45:58 -0400

    file parsing & input file

--- src/main.py
+++ src/main.py
@@ -1,7 +1,27 @@
 
 
 def main():
-    print("hi")
+    #reading from file
+    try:
+        #alphabet parsing
+        with open("data/example.in","r") as file:
+            #reads each line into array lines
+            lines= [line.strip() for line in file if line.strip()]
+        #1st line becomes int k (alphabet length)
+        k=int(lines[0])
+        #empty dictionary
+        values={}
+        for i in range(1,k+1):
+            char,value = lines[i].split()
+            values[char]=int(value)
+        A=lines[k+1]
+        B=lines[k+2]
+        print(values)
+        print(A)
+        print(B)
+    except FileNotFoundError:
+        print("File opening error")
+        return
 
 if __name__ == "__main__":
     main()
